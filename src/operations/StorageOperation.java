9700514d8c84f64e25edace2b78ba1dc082e7fe2
VibesBG <dimitarvaleri05@gmail.com>
2026-05-10 22:53:01 +0300

    add interface StorageOperation

--- src/operations/StorageOperation.java
+++ src/operations/StorageOperation.java
@@ -1,3 +1,10 @@
+package operations;
+
+import model.LogEntry;
+import model.Product;
+import java.util.List;
+import java.util.Map;
+
 public interface StorageOperation {
     String execute(Map<String, List<Product>> stock, List<LogEntry> log);
 }
